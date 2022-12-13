78095437f6978f1761dc4677042d24a554f55e3a
RafaSiuda <118115213+RafaSiuda@users.noreply.github.com>
2022-12-13 00:52:05 -0300

    Acrescentei arquivo rar

---
+++ if/if01.js
@@ -0,0 +1,9 @@
+function notaBoa (nota) {
+    if (nota >=8){
+        console.log('Você foi aprovado com a nota ' + nota)
+    } else{
+        console.log('Você foi reprovado com a nota ' + nota)
+    }
+}
+
+notaBoa(10)
